c5da59204c5b89906c33233085b5e8f891111a78
Rakib Hasan <rakibhasan1808@gmail.com>
2026-09-08 16:13:06 +0600

    Added new page name of "Job Serial Format"

---
+++ MerchandisingMarketing/BasicSetup/JobSerialFormat.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Nexa_ERP.MerchandisingMarketing.BasicSetup
+{
+    public partial class JobSerialFormat : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // TODO: load current active pattern + "same for PO" selection
+                // and set rbActive1/2/3, rbSameForPO1/2/3 accordingly
+            }
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            // TODO: determine which rbActiveX / rbSameForPOX is checked
+            // and save the selected Job Serial Format pattern
+        }
+    }
+}
